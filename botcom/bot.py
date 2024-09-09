af9bc8d62919fd9df34972e3bc5bed3829940477
beckyshaz <sharonsakah8@gmail.com>
2024-09-09 05:03:26 -0700

    quering database for data

--- botcom/bot.py
+++ botcom/bot.py
@@ -1,8 +1,17 @@
 from flask import Flask, request
 from flask import requests
 from twilio.twiml.messaging_response import MessagingResponse
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
+
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['PASSWORD'] = ''
+app.config['DATABASE'] = 'eshop'
+
+mysql = MySQL(app)
 
 @app.route('/botcom', methods=['POST'])
 def botcom():
@@ -11,7 +20,12 @@ def botcom():
     resp = MessagingResponse()
     msg = resp.message()
 
+    if 'hello' or 'hi' in incoming_msg:
+        msg.body = "hello, how my I help you today?"
+    if 'browse products' or 'show goods' in incoming_msg:
+        cursor = mysql.connection.cursor()
 
+        cursor.execute(("SELECT * FROM product WHERE quantity_available > 0")
 
 
 
