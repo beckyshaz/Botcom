8534891eca5be92b8887d56f0ab1c462daab5ac4
beckyshaz <sharonsakah8@gmail.com>
2024-09-13 05:46:12 -0700

    modified

--- botcom/botcom.sql
+++ botcom/botcom.sql
@@ -1,15 +1,15 @@
 -- creating a database using create database
-CREATE DATABASE IF NOT EXISTS eshop;
-USE eshop;
+CREATE DATABASE IF NOT EXISTS eshops;
+USE eshops;
 
 CREATE TABLE IF NOT EXISTS users(
+user_id INT AUTO_INCREMENT PRIMARY KEY,
 first_name VARCHAR(50) NOT NULL,
 second_name VARCHAR(50) NOT NULL,
 username VARCHAR(50) NOT NULL,
 email VARCHAR(100) NOT NULL,
 password_hash VARCHAR(255) NOT NULL,
 phone_number VARCHAR(100),
-PRIMARY KEY (phone_number),
 UNIQUE (username),
 UNIQUE (email)
 );
@@ -24,9 +24,9 @@ CREATE TABLE IF NOT EXISTS product (
 
 CREATE TABLE IF NOT EXISTS cart (
 	cart_id INT AUTO_INCREMENT PRIMARY KEY,
-	phone_number VARCHAR(100),
+	user_id INT,
 	total_amount DECIMAL (10, 2),
-	FOREIGN KEY (phone_number) REFERENCES users(phone_number) ON DELETE CASCADE	
+	FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE	
 );
 
 CREATE TABLE IF NOT EXISTS cartitem (
@@ -42,9 +42,9 @@ CREATE TABLE IF NOT EXISTS cartitem (
 
 INSERT INTO product (product_name, description, price, quantity_available)
 VALUES
-    ('Smartphone A', 'Very smart and easy to use phone', 19.99, 50),
-    ('Smartphone B', 'Unique and waterproof', 20.99, 100),
-    ('Headphones B', 'Protects your eardrum', 5.00, 1000),
-    ('Television C', 'Smart LED TVs', 300.00, 1000),
-    ('Computer D', 'High performing computers', 400.00, 1000);
+    ('SmartphoneA', 'Very smart and easy to use phone', 19.99, 50),
+    ('SmartphoneB', 'Unique and waterproof', 20.99, 100),
+    ('HeadphoneC', 'Protects your eardrum', 5.00, 1000),
+    ('TelevisionD', 'Smart LED TVs', 300.00, 1000),
+    ('ComputerE', 'High performing computers', 400.00, 1000);
 
